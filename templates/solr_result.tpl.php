0d93eada11cea8baf26251f7a615b454bf719677
Adam <adam.vessey@gmail.com>
2011-07-17 21:29:18 -0300

    A little progress...
    About to refactor a bit change stuff somewhat...  Render the different
    content types together, so all the composers displayed will be lumped
    together in one table, and all a table rendered for each type, if there
    are any rows for that type...  Or such is the plan.

--- templates/solr_result.tpl.php
+++ templates/solr_result.tpl.php
@@ -1,10 +1,16 @@
 <?php
 dsm($item);
+$titn_addr = 'http://www.march.es/abnopac/abnetcl.exe?ACC=DOSEARCH&xsqf01=';
 ?>
 <div class="atm_solr_result">
 <!--<p><? print $item['type'] ?></p>-->
 <?
 switch($item['type']):
+case "Performance":?>
+	<h3><? t('Title:') ?>
+	<p><? print $item['title'] ?></p>
+	<p><? print $item['asdf'] ?></p>
+<?	break;
 case "Performer":?>
 	<p><? print $item['name'] ?></p>
 	<p><? print $item['piece'] ?></p>
@@ -26,6 +32,33 @@ case "Composer":
 		)
 	);
 	break;
+case "Score":?>
+	<h2><? print t("Piece:") ?></h2>
+	<h3><? print t('Title:') ?></h3>
+	<p><? print $item['title']; ?></p>
+	<h3><? print t("Composer:") ?></h3>
+	<p><? print $item['composer'] ?></p>
+	<h3><? print t("Score:") ?></h3>
+	<p><?
+	if($item['pdf'])
+	{
+		print l(t("PDF"), 'fedora/repository/' . $item['PID']);
+	}
+	else
+	{
+		print t("No PDF");
+	}?></p>
+	<p><?
+	if($item['titn'] > 0)
+	{
+		print l(t('Available in library'), $titn_addr . $item['titn']);
+	}
+	else
+	{
+		print t('Not in library!');
+	}
+	?></p>
+<?	break;
 default:?>
 	<p>Display not implemented for this type!</p>
 <?endswitch;?>
