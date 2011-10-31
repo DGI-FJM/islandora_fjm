929f5f5d2f3ee5c2eab5ceef8ff71877fc4a9c89
Adam <adam.vessey@gmail.com>
2011-10-31 19:19:32 -0300

    Tweak concert diplay markup for themeing.

--- templates/atm_concert.tpl.php
+++ templates/atm_concert.tpl.php
@@ -10,41 +10,48 @@ drupal_add_css("$islandoraPath/css/islandora_fjm_concert.css");
         <h3 class="atm_concert_cycle"><? echo $concert['cycle'] ?></h3>
         <p class="atm_concert_date"><? echo format_date($concert['date']->format('U'), 'custom', 'd/m/Y') ?></p>
         <p class="atm_concert_description"><? echo $concert['description'] ?></p>
-   </div><!--atm_con_top_right -->
-   <div class="atm_concert_top_right">
-       <div style="float: left; padding: 15px;">
-           <h3><? echo t('Program PDF:')?></h3>
-           <p>
-               <? 
-                   if ($concert['program']['pid']) { 
-                       echo l(t('Available'), 'fedora/repository/' . $concert['program']['pid'], array('attributes' => array('class' => 'pdf')));
-                   }
-                   else {
-                       echo t('N/A');
-                   }
-               ?>
-           </p>
-       </div>
-   </div>
-</div><!--atm_con_top -->
-<div class="atm_concert_bottom">
-    <div class="atm_concert_bottom_left">
-        <?
-            //TODO:  Need to (better) determine whether or not to show the player...  Or just always show it?
-            if (sizeof($concert['performance_rows']) + sizeof($concert['lecture_rows']) > 0)
-            {
-                echo FJM::addPlayer('div.atm_track');
-                //echo theme('islandora_fjm_atm_flowplayer', 'div.atm_track');
-                drupal_add_css("$islandoraPath/css/islandora_fjm_playlist.css");
-            }
-            if (sizeof($concert['performance_rows']) > 0) {
-                echo theme('table', $concert['headers']['performance'], $concert['performance_rows'], array('class' => 'atm_concert_performance_table'), t('Works'));
-            }
-            if (sizeof($concert['lecture_rows']) > 0) {
-                echo theme('table', $concert['headers']['lecture'], $concert['lecture_rows'], array('class' => 'atm_concert_lecture_table'), t('Conferences'));
-            }
+        <div id="flowplayer">
+        <? 
+        //TODO:  Need to (better) determine whether or not to show the player...  Or just always show it?
+        if (sizeof($concert['performance_rows']) + sizeof($concert['lecture_rows']) > 0)
+        {
+            echo FJM::addPlayer('div.atm_track');
+            //echo theme('islandora_fjm_atm_flowplayer', 'div.atm_track');
+            drupal_add_css("$islandoraPath/css/islandora_fjm_playlist.css");
+        }
         ?>
-    </div><!--bottom left-->
+        </div>
+   </div><!--atm_con_top_right -->
+   <div class="clearfix"></div>
+</div>
+<div class="atm_concert_mid">
+  <h3><? echo t('Program PDF:')?></h3>
+  <p>
+   <? 
+     if ($concert['program']['pid']) { 
+       echo l(t('Available'), 'fedora/repository/' . $concert['program']['pid'], array('attributes' => array('class' => 'pdf')));
+     }
+     else {
+       echo t('N/A');
+     }
+   ?>
+  </p>
+</div> <!-- atm_con_mid -->
+<div class="atm_concert_bottom">
+  <div class="atm_concert_bottom_left">
+    <!-- What's supposed to go here again? -->
+  </div>
+  <div class="atm_concert_bottom_right">
+    <?
+      if (sizeof($concert['performance_rows']) > 0) {
+          echo theme('table', $concert['headers']['performance'], $concert['performance_rows'], array('class' => 'atm_concert_performance_table'), t('Works'));
+      }
+      if (sizeof($concert['lecture_rows']) > 0) {
+          echo theme('table', $concert['headers']['lecture'], $concert['lecture_rows'], array('class' => 'atm_concert_lecture_table'), t('Conferences'));
+      }
+    ?>
+  </div><!--bottom right-->
+  <div class="clearfix"></div>
 </div>
 <? if ($pagenumber != NULL && $pagenumber > 0) : ?>
 <script type="text/javascript">
