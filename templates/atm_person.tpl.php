f8a1783b25a10271a2ec2a828f93137e3fcc4b6c
Adam <adam.vessey@gmail.com>
2011-10-31 11:44:21 -0300

    Fix composer page, as per: http://support.discoverygarden.ca/issues/1492

--- templates/atm_person.tpl.php
+++ templates/atm_person.tpl.php
@@ -2,9 +2,14 @@
 /**
  * Template for a person/composer
  */
-//dsm($date);
-print theme('islandora_fjm_atm_imagegallery', $pid);
 ?>
+<div class='leftcolumn'>
+<?
+echo theme('islandora_fjm_atm_imagegallery', $pid);
+echo theme('table', $performance_headers, $performances['associated']);
+?>
+</div>
+<div class='rightcolumn'>
 <h2><? echo $name['first'] . " " . $name['last'] ?></h2>
 <h3>
     <? echo ((!empty($date['birth']) && $date['birth'] !== FALSE) ? ($date['birth']) : t('unknown'))
@@ -14,6 +19,4 @@ print theme('islandora_fjm_atm_imagegallery', $pid);
     <? echo $biography ?>
 </div>
 <h3><? echo t("Performances in CLAMOR") ?></h3>
-<? 
-print theme('table', $performance_headers, $performances['associated']);
-?>
+</div>
