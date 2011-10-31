d9f4caf47087ce6d6bab5b566942a45ebdfc36a0
Adam <adam.vessey@gmail.com>
2011-10-31 18:48:50 -0300

    A bit of general clean up...
    ...  Comment out some DSMs, fix the printing of Solr module debug
    messages.

--- templates/atm_person.tpl.php
+++ templates/atm_person.tpl.php
@@ -6,8 +6,10 @@
 <div class='leftcolumn'>
 <?
 echo theme('islandora_fjm_atm_imagegallery', $pid);
+?><h3><? echo t("Performances in CLAMOR") ?></h3>
+<div class='performance_table'><?
 echo theme('table', $performance_headers, $performances['associated']);
-?>
+?></div>
 </div>
 <div class='rightcolumn'>
 <h2><? echo $name['first'] . " " . $name['last'] ?></h2>
@@ -18,5 +20,5 @@ echo theme('table', $performance_headers, $performances['associated']);
 <div class="fjm_person_bio">
     <? echo $biography ?>
 </div>
-<h3><? echo t("Performances in CLAMOR") ?></h3>
+
 </div>
