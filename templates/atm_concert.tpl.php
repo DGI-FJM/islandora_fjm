54a11702cc00646f0fcb9024dab434534cb57900
Adam <adam.vessey@gmail.com>
2011-11-01 10:56:10 -0300

    Tweak concert markup

--- templates/atm_concert.tpl.php
+++ templates/atm_concert.tpl.php
@@ -3,13 +3,16 @@ drupal_add_css("$islandoraPath/css/islandora_fjm_concert.css");
 drupal_set_title(t('Concerts'));
 //dsm($concert);
 ?>
-<h2 class="atm_concert_title"><? echo $concert['title'] ?></h2>
-<div class="atm_concert_top">
-    <? echo theme('islandora_fjm_atm_imagegallery', $pid) ?>
-    <div class="atm_concert_top_right">
-        <h3 class="atm_concert_cycle"><? echo $concert['cycle'] ?></h3>
-        <p class="atm_concert_date"><? echo format_date($concert['date']->format('U'), 'custom', 'd/m/Y') ?></p>
-        <p class="atm_concert_description"><? echo $concert['description'] ?></p>
+<div class="islandora_fjm_concert">
+  
+  <div class="atm_concert_top">
+      <? echo theme('islandora_fjm_atm_imagegallery', $pid) ?>
+      <div class="atm_concert_top_right">
+        <div class="islandora_fjm_description">
+          <h3 class="atm_concert_cycle"><? echo $concert['cycle'] ?></h3>
+          <p class="atm_concert_date"><? echo format_date($concert['date']->format('U'), 'custom', 'd/m/Y') ?></p>
+          <p class="atm_concert_description"><? echo $concert['description'] ?></p>
+        </div>
         <div id="flowplayer">
         <? 
         //TODO:  Need to (better) determine whether or not to show the player...  Or just always show it?
@@ -21,40 +24,41 @@ drupal_set_title(t('Concerts'));
         }
         ?>
         </div>
-   </div><!--atm_con_top_right -->
-   <div class="clearfix"></div>
-</div>
-<div class="atm_concert_mid">
-  <h3><? echo t('Program PDF:')?></h3>
-  <p>
-   <? 
-     if ($concert['program']['pid']) { 
-       echo l(t('Available'), 'fedora/repository/' . $concert['program']['pid'], array('attributes' => array('class' => 'pdf')));
-     }
-     else {
-       echo t('N/A');
-     }
-   ?>
-  </p>
-</div> <!-- atm_con_mid -->
-<div class="atm_concert_bottom">
-  <div class="atm_concert_bottom_left">
-    <!-- What's supposed to go here again? -->
+     </div><!--atm_con_top_right -->
+     <div class="clearfix"></div>
   </div>
-  <div class="atm_concert_bottom_right">
-    <?
-      if (sizeof($concert['performance_rows']) > 0) {
-          echo theme('table', $concert['headers']['performance'], $concert['performance_rows'], array('class' => 'atm_concert_performance_table'), t('Works'));
-      }
-      if (sizeof($concert['lecture_rows']) > 0) {
-          echo theme('table', $concert['headers']['lecture'], $concert['lecture_rows'], array('class' => 'atm_concert_lecture_table'), t('Conferences'));
-      }
-    ?>
-  </div><!--bottom right-->
-  <div class="clearfix"></div>
+  <div class="atm_concert_mid">
+    <h3><? echo t('Program PDF:')?></h3>
+    <p>
+     <? 
+       if ($concert['program']['pid']) { 
+         echo l(t('Available'), 'fedora/repository/' . $concert['program']['pid'], array('attributes' => array('class' => 'pdf')));
+       }
+       else {
+         echo t('N/A');
+       }
+     ?>
+    </p>
+  </div> <!-- atm_con_mid -->
+  <div class="atm_concert_bottom">
+    <div class="atm_concert_bottom_left">
+      <!-- What's supposed to go here again? -->
+    </div>
+    <div class="atm_concert_bottom_right">
+      <?
+        if (sizeof($concert['performance_rows']) > 0) {
+            echo theme('table', $concert['headers']['performance'], $concert['performance_rows'], array('class' => 'atm_concert_performance_table'), t('Works'));
+        }
+        if (sizeof($concert['lecture_rows']) > 0) {
+            echo theme('table', $concert['headers']['lecture'], $concert['lecture_rows'], array('class' => 'atm_concert_lecture_table'), t('Conferences'));
+        }
+      ?>
+    </div><!--bottom right-->
+    <div class="clearfix"></div>
+  </div>
+  <? if ($pagenumber != NULL && $pagenumber > 0) : ?>
+  <script type="text/javascript">
+      $(function() {$(".concertOrder_<? echo $pagenumber ?>:first > a").click()});
+  </script>
+  <?endif;?>
 </div>
-<? if ($pagenumber != NULL && $pagenumber > 0) : ?>
-<script type="text/javascript">
-    $(function() {$(".concertOrder_<? echo $pagenumber ?>:first > a").click()});
-</script>
-<?endif;?>
--- templates/imagegallery.tpl.php
+++ templates/imagegallery.tpl.php
@@ -18,39 +18,43 @@ $first = true;
 foreach ($images as $image): 
     if($first):
         $first = false;?>
-    <a href="<? echo $image['path']?>" rel="lightbox" id="image_strip_link" title="<? echo $image['alt'] ?>">
-        <img id="image_strip_big" src="<? echo $image['path']?>" title="<? echo $image['alt']?>"></img>
-    </a>
-    
-    <ul class="image_strip">
-<?  endif;
-    if(sizeof($images) > 1):?>
-        <li>
-            <img src="<? echo $image['thumbnail']?>" title="<? echo $image['alt']?>" path="<? echo $image['path']?>"></img>
-        </li>
-<?  endif;
-    endforeach; ?>
-    </ul>
-<? if(sizeof($images) > 1):?>
-    <script type="text/javascript">
-        //FIXME (minor): Might be a good idea to get this JS out of here? 
-        //  (into another file, that is...)
-        $("ul.image_strip > li > img").click(function () {
-            $("#image_strip_big").attr("src", $(this).attr("path"));
-            $("#image_strip_big").attr("title", $(this).attr("title"));
-            $("#image_strip_link").attr("title", $(this).attr("title"));
-            $("#image_strip_link").attr("href", $(this).attr("path"));
-        });
-    </script>
-    <? jcarousel_add("image_strip", 
-            array(
-                'start' =>  "1", 
-                'wrap' => "both", 
-                'visible' => 3, 
-                'vertical' => FALSE, 
-                'itemFallbackDimension' => 300
-            )
-        );
-    endif;
-    lightbox2_add_files(); ?>
+  <div class="islandora_fjm_photo">
+      <a href="<? echo $image['path']?>" rel="lightbox" id="image_strip_link" title="<? echo $image['alt'] ?>">
+          <img id="image_strip_big" src="<? echo $image['path']?>" title="<? echo $image['alt']?>"></img>
+      </a>
+  </div>
+  <div class="islandora_fjm_jcarousel">
+
+      <ul class="image_strip">
+  <?  endif;
+      if(sizeof($images) > 1):?>
+          <li>
+              <img src="<? echo $image['thumbnail']?>" title="<? echo $image['alt']?>" path="<? echo $image['path']?>"></img>
+          </li>
+  <?  endif;
+      endforeach; ?>
+      </ul>
+  <? if(sizeof($images) > 1):?>
+      <script type="text/javascript">
+          //FIXME (minor): Might be a good idea to get this JS out of here? 
+          //  (into another file, that is...)
+          $("ul.image_strip > li > img").click(function () {
+              $("#image_strip_big").attr("src", $(this).attr("path"));
+              $("#image_strip_big").attr("title", $(this).attr("title"));
+              $("#image_strip_link").attr("title", $(this).attr("title"));
+              $("#image_strip_link").attr("href", $(this).attr("path"));
+          });
+      </script>
+      <? jcarousel_add("image_strip", 
+              array(
+                  'start' =>  "1", 
+                  'wrap' => "both", 
+                  'visible' => 3, 
+                  'vertical' => FALSE, 
+                  'itemFallbackDimension' => 300
+              )
+          );
+      endif;
+      lightbox2_add_files(); ?>
+  </div>
 </div>
