7ba47eddae8d5f104583c2f83ccc37b3e7d4415b
Adam <adam.vessey@gmail.com>
2011-11-01 11:02:18 -0300

    Tweak markup:  Add top-level div w/ class

--- templates/atm_person.tpl.php
+++ templates/atm_person.tpl.php
@@ -4,22 +4,24 @@
  */
 drupal_set_title(t('Composers'));
 ?>
-<div class='leftcolumn'>
-<?
-echo theme('islandora_fjm_atm_imagegallery', $pid);
-?><div class='performance_table'>
-<h3><? echo t("Performances in CLAMOR") ?></h3><?
-echo theme('table', $performance_headers, $performances['associated']);
-?></div>
-</div>
-<div class='rightcolumn'>
-<h2><? echo $name['first'] . " " . $name['last'] ?></h2>
-<h3>
-    <? echo ((!empty($date['birth']) && $date['birth'] !== FALSE) ? ($date['birth']) : t('unknown'))
-        . ' - ' . ((!empty($date['death']) && $date['death'] !== FALSE) ? ($date['death']) : ''); ?>
-</h3>
-<div class="fjm_person_bio">
-    <? echo $biography ?>
-</div>
+<div class="islandora_fjm_person">
+  <div class='leftcolumn'>
+  <?
+  echo theme('islandora_fjm_atm_imagegallery', $pid);
+  ?><div class='performance_table'>
+  <h3><? echo t("Performances in CLAMOR") ?></h3><?
+  echo theme('table', $performance_headers, $performances['associated']);
+  ?></div>
+  </div>
+  <div class='rightcolumn'>
+  <h2><? echo $name['first'] . " " . $name['last'] ?></h2>
+  <h3>
+      <? echo ((!empty($date['birth']) && $date['birth'] !== FALSE) ? ($date['birth']) : t('unknown'))
+          . ' - ' . ((!empty($date['death']) && $date['death'] !== FALSE) ? ($date['death']) : ''); ?>
+  </h3>
+  <div class="fjm_person_bio">
+      <? echo $biography ?>
+  </div>
 
+  </div>
 </div>
--- templates/atm_score.tpl.php
+++ templates/atm_score.tpl.php
@@ -7,25 +7,26 @@
      */
 drupal_set_title(t('Scores'));
 ?>
-
-<h2><? echo $title ?></h2>
-<h3><? echo $composer ?></h3>
-<? echo theme('islandora_fjm_flexpaper', $pid) ?>
-<div class="islandora_fjm_download">
-    <? echo l(t('Download PDF'), "fedora/repository/$pid/PDF/download/" . t("Score") . " - $title.pdf") ?>
-</div>
-<div class="islandora_fjm_description">
-<? if(!empty($abstract)): ?>
-    <h4><? echo t("Abstract") ?></h4>
-    <p><? echo $abstract ?></p>
-<? endif; ?>
-<? 
-if(sizeof($notes) > 0): ?>
-    <h4><? echo t("Notes") ?></h4>
-<?foreach($notes as $note): ?>
-    <p><? echo $note ?></p>
-<? 
-endforeach; 
-endif;
-?>
+<div class="islandora_fjm_score">
+  <h2><? echo $title ?></h2>
+  <h3><? echo $composer ?></h3>
+  <? echo theme('islandora_fjm_flexpaper', $pid) ?>
+  <div class="islandora_fjm_download">
+      <? echo l(t('Download PDF'), "fedora/repository/$pid/PDF/download/" . t("Score") . " - $title.pdf") ?>
+  </div>
+  <div class="islandora_fjm_description">
+  <? if(!empty($abstract)): ?>
+      <h4><? echo t("Abstract") ?></h4>
+      <p><? echo $abstract ?></p>
+  <? endif; ?>
+  <? 
+  if(sizeof($notes) > 0): ?>
+      <h4><? echo t("Notes") ?></h4>
+  <?foreach($notes as $note): ?>
+      <p><? echo $note ?></p>
+  <? 
+  endforeach; 
+  endif;
+  ?>
+  </div>
 </div>
