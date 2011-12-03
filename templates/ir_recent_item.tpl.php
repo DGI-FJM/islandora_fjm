eb25fbbe9e9418b52245f466dd93330a27a46390
Adam <adam.vessey@gmail.com>
2011-12-02 20:12:29 -0400

    Create 'recent additions' block

---
+++ templates/ir_recent_item.tpl.php
@@ -0,0 +1,9 @@
+<?php //blah ?>
+<tr>
+  <th scope="row">
+    <div class="icon type-<? echo $type; ?>">&nbsp;</div>
+  </th>
+  <td headers="itemcolumn col1">
+    <a href="<? echo $link; ?>"><? echo $citation; ?></a></td>
+  <td headers="itemcolumn col2">
+    <div class="icon access-<? echo $access; ?>">&nbsp;</div></td></tr>
