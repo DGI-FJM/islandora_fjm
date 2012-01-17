507900c4b2dcb71a322cf63b78cc345edfd9b650
Adam Vessey <adam.vessey@gmail.com>
2012-01-17 15:31:58 -0400

    Fix (hopefully) Atom template.

--- templates/islandora-fjm-atomfeed.tpl.php
+++ templates/islandora-fjm-atomfeed.tpl.php
@@ -1,6 +1,8 @@
-<?php ?>
+<?php echo <<<XML_START
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+XML_START;
+?>
 <feed xmlns="http://www.w3.org/2005/Atom">
   <id><?php echo $id; ?></id>
   <link href="<?php ?>" rel="self"/>
