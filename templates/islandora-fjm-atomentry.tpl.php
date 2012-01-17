dc7c4a537b194db847130c5b96950d6a65ee080f
Adam Vessey <adam.vessey@gmail.com>
2012-01-17 11:17:29 -0400

    Implement Atom feed

---
+++ templates/islandora-fjm-atomentry.tpl.php
@@ -0,0 +1,29 @@
+<?php ?>
+  <entry>
+    <id><?php echo $id; ?></id>
+    <title><?php echo $title; ?></title>
+<?php if (!empty($published)): ?>
+    <published><?php echo $published; ?></published>
+<?php endif;
+if (!empty($updated)): ?>
+    <updated><?php echo $updated; ?></updated>
+<?php endif; 
+if (!empty($link)):?>
+    <link href="<?php echo $link; ?>"/>
+<?php endif;
+if (!empty($content)): ?>
+    <content type="xhtml">
+      <div xmlns="http://www.w3.org/1999/xhtml"><?php echo $content; ?></div>
+    </content>
+<?php endif;
+  foreach ($authors as $author):?>
+    <author>
+      <name><?php echo $author; ?></name>
+    </author>
+<?php endforeach; 
+  foreach ($contributors as $contributor):?>
+    <contributor>
+      <name><?php echo $contributor; ?></name>
+    </contributor>
+<?php endforeach;?>
+  </entry>
---
+++ templates/islandora-fjm-atomfeed.tpl.php
@@ -0,0 +1,19 @@
+<?php ?>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <id><?php echo $id; ?></id>
+  <link href="<?php ?>" rel="self"/>
+  <title><?php echo $title; ?></title>
+<?php if (!empty($subtitle)):?>
+  <subtitle><?php echo $subtitle; ?></subtitle>
+<?php endif; 
+foreach ($entries as $entry) {
+  echo $entry;
+}?>
+</feed>
+<?php 
+if (!headers_sent()) {
+  header('Content-Type: application/atom+xml; charset=UTF-8');
+}
+exit(); ?>
