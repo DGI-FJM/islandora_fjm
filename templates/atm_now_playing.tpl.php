743ce8e28561a2138ce6dc686e4011eb022b9866
Adam Vessey <adam.vessey@gmail.com>
2012-02-16 16:25:22 -0400

    Split into submodules...  Still some more to do.

--- templates/atm_now_playing.tpl.php
+++
@@ -1,9 +0,0 @@
-<h2 class='player-content'><?php echo $piece ?></h2><br/><?php 
-if (count($players) > 0): 
-  ?><p class='player-content'><?php 
-  foreach($players as $name => $instruments):
-    $built[] = "$name: (" . implode(', ', $instruments) . ")";
-  endforeach; 
-  echo implode(', ', $built);
-?></p><?php
-endif;?>
